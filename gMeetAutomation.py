bdb142166c81f99ac26c9c803ff69c53600af5c9
AdritoPramanik <74486413+AdritoPramanik@users.noreply.github.com>
2021-04-28 07:55:02 +0530

    Add files via upload

---
+++ gMeetAutomation.py
@@ -0,0 +1,31 @@
+import pyautogui
+import time
+
+meetingID = pyautogui.prompt(text='Enter your meeting ID', title='Meeting ID', default='')
+time.sleep(1)
+pyautogui.press('win', interval=0.5)
+time.sleep(1)
+pyautogui.typewrite('chrome', interval=0.5)
+time.sleep(1)
+pyautogui.press('enter', interval=0.5)
+time.sleep(13)
+pyautogui.typewrite('https://meet.google.com/?authser=0', interval=0.3)
+time.sleep(1)
+pyautogui.press('enter', interval=0.5)
+time.sleep(13)
+pyautogui.click(250, 520)
+time.sleep(1)
+pyautogui.typewrite(meetingID, interval=0.2)
+time.sleep(1)
+pyautogui.press('enter', interval=0.2)
+time.sleep(9)
+pyautogui.click(400, 570)
+time.sleep(2)
+pyautogui.click(500, 570)
+time.sleep(2)
+pyautogui.alert(text='We are entering a meeting',  title='Info', button='OK')
+time.sleep(1)
+pyautogui.click(990, 440)
+print("Asked to join")
+
+
